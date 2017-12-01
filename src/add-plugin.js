c0891c3eeef7d553150693b8240a0cab28374944
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-30 22:14:06 -0500

    fix path

--- src/add-plugin.js
+++ src/add-plugin.js
@@ -8,8 +8,12 @@ shell.set('-v')
 
 // TODO copy previous file if exists to .bak
 
-const root = path.join(__dirname, '..', '..')
+// during NPM post install phase it is running in
+// node_modules/@bahmutov/add-typescript-to-cypress
+const root = path.join(process.cwd(), '..', '..', '..')
 const plugins = path.join(root, 'cypress', 'plugins')
 const pluginsIndex = path.join(plugins, 'index.js')
 
-shell.cp('./plugin.js', pluginsIndex)
+const sourcePlugin = path.join(__dirname, 'plugin.js')
+
+shell.cp(sourcePlugin, pluginsIndex)
